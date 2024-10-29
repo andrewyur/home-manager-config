ad7fdaddd8271d05f05b31aef73d32438bfa004e
Home <home@andrew-laptop.local>
2024-10-29 11:12:18 -0400

    initial commit

---
+++ home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  programs.zsh.enable = true;
+  programs.zsh.oh-my-zsh = {
+    enable = true;
+    plugins = [
+      "git"
+      "sudo"
+      "direnv"
+    ];
+    theme = "robbyrussell";
+  };
+
+  programs.fzf.enable = true;
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  home.username = "home";
+  home.homeDirectory = "/Users/home";
+
+  home.packages = [
+    pkgs.git
+  ];
+
+  home.file = {
+    # this will probably change pretty frequently, i need to make sure to update it
+    "config/zed/settings.json".text = ''
+    {
+      "ui_font_size": 16,
+      "buffer_font_size": 12,
+      "theme": {
+        "mode": "system",
+        "light": "Rosé Pine Dawn",
+        "dark": "One Dark"
+      },
+      "terminal": {
+        "working_directory": "current_project_directory"
+      }
+    }
+    ''
+  }
+
+  home.sessionVariables = {
+    EDITOR = "zed";
+
+    # Direnv log formatting
+    # DIRENV_LOG_FORMAT="$'\033[2mdirenv: %s\033[0m'";
+    DIRENV_LOG_FORMAT="";
+  };
+
+  # DONT CHANGE
+  home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
