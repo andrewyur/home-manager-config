7ba2944919e4ae07e0027e0b0c319b2e2ff823cf
Home <home@andrew-laptop.local>
2024-10-29 14:16:24 -0400

    remove zed settings

--- home.nix
+++ home.nix
@@ -28,25 +28,10 @@
   ];
 
   home.file = {
-    # this will probably change pretty frequently, i need to make sure to update it
-    ".config/zed/settings.json".text = ''
-    {
-      "ui_font_size": 16,
-      "buffer_font_size": 12,
-      "theme": {
-        "mode": "system",
-        "light": "Rosé Pine Dawn",
-        "dark": "One Dark"
-      },
-      "terminal": {
-        "working_directory": "current_project_directory"
-      }
-    }
-    '';
     ".config/1password/ssh/agent.toml".text = ''
         [[ssh-keys]]
         vault = "Andy's Cool Stuff"
-    ''
+    '';
   };
 
   home.sessionVariables = {
